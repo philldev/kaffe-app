838e2b8c31faa6c82dca21a1fd69813afbe97842
Deddy Wolley <deddywolley@gmail.com>
2023-08-14 14:49:50 +0800

    add Order utils

---
+++ src/utils/order-utils.ts
@@ -0,0 +1,31 @@
+import { Order } from "@/types/order";
+
+export function getStatusLabel(status: Order["status"]) {
+  switch (status) {
+    case "new":
+      return "New";
+    case "canceled":
+      return "Canceled";
+    case "completed":
+      return "Completed";
+    case "waiting_for_payment":
+      return "Waiting for payment";
+    default:
+      return "";
+  }
+}
+
+export function getStatusTwBgColor(status: Order["status"]) {
+  switch (status) {
+    case "new":
+      return "bg-yellow-500";
+    case "canceled":
+      return "bg-red-500";
+    case "completed":
+      return "bg-green-600";
+    case "waiting_for_payment":
+      return "bg-blue-500";
+    default:
+      return "bg-background";
+  }
+}
