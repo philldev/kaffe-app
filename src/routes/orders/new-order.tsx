4ff8338dd8334bc10e1cbc8851beef773349f121
Deddy Wolley <deddywolley@gmail.com>
2023-08-15 01:41:22 +0800

    add "new order" base ui

--- src/routes/orders/new-order.tsx
+++ src/routes/orders/new-order.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { OverlaySpinner } from "@/components/ui/spinner";
+import { createArray } from "@/lib/utils";
 import { ChevronLeftIcon } from "@radix-ui/react-icons";
+import { IconMinus, IconPlus, IconTrash } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
 export const NewOrderPage = () => {
@@ -21,12 +25,52 @@ export const NewOrderPage = () => {
         </div>
       </div>
       <ScrollArea className="flex-1">
-        <div className="py-2 px-4"></div>
+        <div className="py-2 px-4 flex flex-col gap-4">
+          <div>
+            <Label>Date</Label>
+            <p className="text-muted-foreground">Order 2202202-001</p>
+          </div>
+          <div>
+            <Label>Order Number</Label>
+            <p className="text-muted-foreground">Order 2202202-001</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Customer name</Label>
+            <Input />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Items</Label>
+            <div className="flex flex-col gap-1">
+              {createArray(5).map((_, index) => (
+                <div key={index} className="flex justify-between gap-2">
+                  <span className="flex-1">Potato x 1</span>
+                  <span>IDR 15K</span>
+                  <div>
+                    <Button variant="ghost" size="icon" className="w-5 h-5">
+                      <IconPlus strokeWidth={1} className="w-3 h-3" />
+                    </Button>
+                    <Button variant="ghost" size="icon" className="w-5 h-5">
+                      <IconMinus strokeWidth={1} className="w-3 h-3" />
+                    </Button>
+                    <Button variant="ghost" size="icon" className="w-5 h-5">
+                      <IconTrash strokeWidth={1} className="w-3 h-3" />
+                    </Button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <Button variant="outline" size="sm" className="w-full">
+              Add Items
+            </Button>
+          </div>
+
+          <div>
+            <Label>Total</Label>
+            <p className="text-muted-foreground text-2xl">IDR 150,000</p>
+          </div>
+        </div>
       </ScrollArea>
       <div className="p-4 flex flex-col gap-4">
-        <Button variant="outline" className="w-full">
-          Add Items
-        </Button>
         <Button className="w-full">Create Order</Button>
       </div>
     </div>
